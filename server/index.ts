c4d3dfbbf7dab84ebb3e5a726526b39ccaa6b6a6
Parth J Chaudhary <parthchaudhary.jc@yahoo.com>
2025-07-11 13:00:13 +0530

    commit

--- server/index.ts
+++ server/index.ts
@@ -50,10 +50,10 @@
     // importantly only setup vite in development and after
     // setting up all the other routes so the catch-all route
     // doesn't interfere with the other routes
-    if (app.get("env") === "development") {
-      await setupVite(app, server);
+    if (process.env.NODE_ENV === "development") {
+      await setupVite(app, server); // for dev mode
     } else {
-      serveStatic(app);
+    serveStatic(app); // for production
     }
 
     // ALWAYS serve the app on port 5000
@@ -67,4 +67,8 @@
     }, () => {
       log(`serving on port ${port}`);
     });
+    console.log("NODE_ENV:", process.env.NODE_ENV);
+    console.log("PORT:", process.env.PORT);
+    
+
   })();
